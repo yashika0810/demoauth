9d58637fc46db8861596b171d737562e5b3fd553
yashika0810 <khatri.yashika48@gmail.com>
2020-03-23 13:29:06 -0400

    authproject

---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+
+class CustomUser(AbstractUser):
+    pass
+
+    #add extra fields 
